258acab715e844c778178a4a29c5066d67e21237
Alexandros Platanios <Alexpl_15@windowslive.com>
2021-09-01 17:24:14 +0300

    v0.0.8

--- Assigments_School/Database_Queries.sql
+++ Assigments_School/Database_Queries.sql
@@ -311,7 +311,7 @@ GO
 /* Edit Assignments */
 /* Insert Assignment */
 GO
-CREATE PROCEDURE UpdateAssignment @title VARCHAR(100), @startdate VARCHAR(30), @enddate VARCHAR(30), @description VARCHAR(500)
+CREATE PROCEDURE InsertAssignment @title VARCHAR(100), @startdate VARCHAR(30), @enddate VARCHAR(30), @description VARCHAR(500)
 AS
 BEGIN
 INSERT INTO Assignments (Title, StartDate, EndDate, Description) VALUES (@title, @startdate, @enddate, @description);
